b523b072ebc94363ba5a74513f3d7f21292af5a5
shiminasai <pablo.ismael90@gmail.com>
2016-10-28 16:27:05 -0600

    cambios en envios de colores

--- static_media/js/colorAdmin.js
+++ static_media/js/colorAdmin.js
@@ -1,5 +1,28 @@
-(function($){
+(function($) {
+    $(document).ready(function($) {
+    	
+    	var x =$("#id_color option").size();
+        for (i = 0; i < x; i++) { 
+		   $("#id_color option[value="+i+"]").css({"background-color":$("#id_color option[value="+i+"]").text(), "color":$("#id_color option[value="+i+"]").text()});
+		}
 
-	$('#id_color').css("background-color", "yellow");
+		 $( "#id_color" )
+		  .change(function () {
+		    var str = "";
+		    $( "#id_color option:selected" ).each(function() {
+		      str += $( this ).text() + " ";
+		    });
+		    $( "#id_color" ).css({"background-color": str, "color": str});
+		  })
+		  .change();
 
-})(jQuery || django.jQuery)
+		  $("#id_color option[value='']").css({"background-color": "#fff", "color": "#fff"});
+    });
+})(jQuery || django.jQuery);
+
+/*(function($) {
+    $(document).ready(function($) {
+    	var color
+        $('#id_color').css("background-color", "yellow");
+    });
+})(jQuery || django.jQuery);*/
